43c82cbdc9669d03e49bf5e553c2ed8316ffbbb0
Arnaud <arnaudkif@gmail.com>
2022-03-28 17:58:57 +0200

    Final commit

--- QuoteTestMarmelade/Controller/QuoteViewController.swift
+++ QuoteTestMarmelade/Controller/QuoteViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class QuoteViewController: UIViewController {
 
-    @IBOutlet var BackgroundView: UIView!
+    var isFirstPart: Bool = true
+    var isSecondPart: Bool = false
+    var isLastPart: Bool = false
+    var progressRate: Int = 0
 
+    @IBOutlet var BackgroundView: UIView!
     @IBOutlet weak var AuthorLabel: UILabel!
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var SmileyImage: UIImageView!
@@ -18,40 +22,68 @@ class QuoteViewController: UIViewController {
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var resultView: UIView!
     @IBOutlet weak var progressSlider: UISlider!
-
+    @IBOutlet weak var quoteButton: UIButton!
+    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var nextbutton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        initView()
-
-        // Do any additional setup after loading the view.
+        self.progressNumber.text = "\(progressRate) % "
+        endButton.isHidden = true
     }
     
+    @IBAction func endbuttonTaped(_ sender: Any) {
+        progressRate = 0
+        quoteButton.isHidden = false
+        endButton.isHidden = true
+    }
+
     @IBAction func NextQuoteButton(_ sender: Any) {
+// TODO: - Lancer la prochaine citation et mettre à jour les labels
+        nextQuote()
+        progressRate += 10
+        progressNumber.text = "\(progressRate) % "
+        progressSlider.value = Float(progressRate)
+        print(progressRate)
+       // TODO: - vérifier le taux et mettre à jour le smiley
+        checkProgress()
     }
 
-    func initView() {
-//        let layer0 = CAGradientLayer()
-//        layer0.colors = [
-//          UIColor(red: 0.953, green: 0.816, blue: 0.329, alpha: 1).cgColor,
-//          UIColor(red: 0.953, green: 0.804, blue: 0.329, alpha: 1).cgColor,
-//          UIColor(red: 0.945, green: 0.761, blue: 0.329, alpha: 1).cgColor,
-//          UIColor(red: 0.937, green: 0.694, blue: 0.325, alpha: 1).cgColor,
-//          UIColor(red: 0.929, green: 0.6, blue: 0.325, alpha: 1).cgColor,
-//          UIColor(red: 0.914, green: 0.478, blue: 0.322, alpha: 1).cgColor,
-//          UIColor(red: 0.898, green: 0.337, blue: 0.318, alpha: 1).cgColor
-//        ]
-//        layer0.locations = [0.07, 0.27, 0.43, 0.59, 0.73, 0.87, 1]
-//        layer0.startPoint = CGPoint(x: 0.25, y: 0.5)
-//        layer0.endPoint = CGPoint(x: 0.75, y: 0.5)
-//        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -1.33, b: -1.17, c: 1.94, d: -0.65, tx: 0.41, ty: 1.49))
-//        layer0.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
-//        layer0.position = BackgroundView.center
-//        BackgroundView.layer.addSublayer(layer0)
+    func checkProgress() {
+        if progressRate > 79 {
+            self.SmileyImage.image = UIImage(named: "smiley_awe.png")
+        }
+        if progressRate < 79 && progressRate > 39 {
+            self.SmileyImage.image = UIImage(named: "smiley_meh.png")
+        }
+        if progressRate < 39 {
+            self.SmileyImage.image = UIImage(named: "smiley_sick.png")
+        }
+        if progressRate == 50 {
+            //TODO: - Lancer une alerte
+            createAlert()
+        }
+        if progressRate == 100 {
+            // TODO: - Terminer la partie, changer le bouton sur finir et repartir à zéro
+            quoteButton.isHidden = true
+            endButton.isHidden = false
+            progressRate = 0
+
+        }
+    }
+
+    func nextQuote() {
+        // TODO: - Lancer l'appel réseau et attribuer la nouvelles citations aux labels correspondants...
 
     }
 
-
-
+    func createAlert() {
+        //TODO: - Finir de paramètrer l'affichage...
+        let alertVC = UIAlertController(title: "Tu es à 50% des citations !", message: "Tu as bientôt fini !", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Arrêter", style: .destructive, handler: nil))
+        alertVC.addAction(UIAlertAction(title: "Continuer", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
 
 }
+
---
+++ QuoteTestMarmelade/Model/Quote.swift
@@ -0,0 +1,15 @@
+//
+//  Quote.swift
+//  QuoteTestMarmelade
+//
+//  Created by arnaud kiefer on 28/03/2022.
+//
+
+import Foundation
+
+struct Quote {
+    let quote: String
+    let author: String
+
+}
+
