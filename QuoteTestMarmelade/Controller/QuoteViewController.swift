8fa56cdb2d502729377aa3be7f3bc4fccba361d5
Arnaud <arnaudkif@gmail.com>
2022-03-30 10:02:18 +0200

    add Gradient on background

--- QuoteTestMarmelade/Controller/QuoteViewController.swift
+++ QuoteTestMarmelade/Controller/QuoteViewController.swift
@@ -11,8 +11,30 @@ class QuoteViewController: UIViewController {
     // MARK: - Class properties
     var progressRate: Int = 0
 
+    // Setting of background gradient
+    lazy var gradient: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        gradient.colors = [
+            UIColor(red: 0.953, green: 0.816, blue: 0.329, alpha: 1).cgColor,
+            UIColor(red: 0.953, green: 0.804, blue: 0.329, alpha: 1).cgColor,
+            UIColor(red: 0.945, green: 0.761, blue: 0.329, alpha: 1).cgColor,
+            UIColor(red: 0.937, green: 0.694, blue: 0.325, alpha: 1).cgColor,
+            UIColor(red: 0.929, green: 0.6, blue: 0.325, alpha: 1).cgColor,
+            UIColor(red: 0.914, green: 0.478, blue: 0.322, alpha: 1).cgColor,
+            UIColor(red: 0.898, green: 0.337, blue: 0.318, alpha: 1).cgColor
+          ]
+        gradient.locations = [0.07, 0.27, 0.43, 0.59, 0.73, 0.87, 1]
+        gradient.startPoint = CGPoint(x: 0.25, y: 0.5)
+        gradient.endPoint = CGPoint(x: 0.75, y: 0.5)
+        gradient.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -1.33, b: -1.17, c: 1.94, d: -0.65, tx: 0.41, ty: 1.49))
+        gradient.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
+        gradient.position = view.center
+        return gradient
+    }()
+
     // MARK: - IBOutlets
-    @IBOutlet var BackgroundView: UIView!
+    @IBOutlet weak var gradientView: UIView!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var smileyImage: UIImageView!
@@ -29,6 +51,7 @@ class QuoteViewController: UIViewController {
         super.viewDidLoad()
         self.progressNumber.text = "\(progressRate) % "
         endButton.isHidden = true
+        gradientView.layer.addSublayer(gradient)
     }
 
     // MARK: - IBActions
